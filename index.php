d6aabb9e79d03cc4dfd8991dc78f66d929beea32
aldinmbrk <aldinmubarok.am@gmail.com>
2021-11-30 00:01:03 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" width="device-width, initial-scale=1.0 shrink-to-fit=no">
+    <title>Meta Inu Fire</title>
+    <link rel="stylesheet" href="fontawesome/css/all.css">
+    <link rel="icon" href="img/fav-metaflokiinu.png" type="text/icon">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- <link rel="stylesheet" href="css/aos.css"> -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<!-- <body data-spy="scroll" data-target="#navbarNav" data-offset="200"> -->
+
+<body data-target="#navbarNav">
+    <!--NAVBAR-->
+    <nav id="main-navbar" class="navbar navbar-expand-md gradient-blue-red fixed-top py-2 shadow">
+        <div class="container">
+            <!-- <div class="d-flex justify-content-center">
+                <a href="index.html" class="navbar-brand text-warning"><img src="img/logo-goldenflokiinu.png" class="mr-2" width="30">GOLDEN FLOKI INU</a>
+            </div> -->
+
+            <a href="index.php" class="navbar-brand text-primary mx-auto"><img src="img/logo-metaflokiinu.png" class="mr-2" width="30"><?= strtoupper('Meta Inu '); ?><span class="red"><?= strtoupper('Fire'); ?></span></a>
+
+            <!-- <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button> -->
+            <!-- <div id="navbarNav" class="collapse navbar-collapse">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="#showcase" class="nav-link p-3 active">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#product" class="nav-link p-3">Product</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#team" class="nav-link p-3">Team</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#blog" class="nav-link p-3">Blog</a>
+                    </li>
+                </ul>
+            </div> -->
+        </div>
+    </nav>
+
+    <div class="fluid-container">
+        <!--SHOWCASE-->
+        <section id="showcase">
+            <div class="dark-overlay">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-10 text-center text-white mx-auto">
+                            <div id="logo">
+                                <img src="img/logo-metaflokiinu.png" width="150">
+                            </div>
+                            <!-- Heading Display on desktop -->
+                            <h1 class="display-3 m-0 blue d-none d-md-block"><strong><?= strtoupper('Meta Inu '); ?><span class="red"><?= strtoupper('Fire'); ?></span></strong></h1>
+                            <!-- Heading Display on mobile -->
+                            <h1 class="display-4 m-0 blue d-md-none"><strong><?= strtoupper('Meta Inu '); ?><span class="red"><?= strtoupper('Fire'); ?></span></strong></h1>
+                            <p class="lead mt-3 d-none d-md-block" style="font-weight: 600;">MetaInuFire is the future. And in the future, MetaInuFire reigns supreme!</p>
+                            <a href="https://pancakeswap.finance/" class="btn btn-lg btn-primary shadow mt-3" style="border-radius: 50px;" target="_blank">BUY ON PANCAKESWAP</a>
+                            <br>
+                            <a href="https://poocoin.app/" class="btn btn-lg btn-primary shadow mt-3" style="border-radius: 50px;" target="_blank">CHART</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- SECTION 1 -->
+        <!-- <section id="showoff-1" class="py-5" data-aos="zoom-out-right"> -->
+        <section id="showoff-1" class="py-5 bg-white">
+            <div class="container">
+                <div class="row">
+                    <div class="col text-center">
+                        <h2 class="font-weight-bold blue"><?= strtoupper('Meta Inu '); ?><span class="red"><?= strtoupper('Fire'); ?></span></h2>
+                        <span class="d-block gradient-blue-red mx-auto mb-3" style="width: 150px; height:2px;"></span>
+                        <p class="mt-4" style="font-size: 18px;">Meta Inu Fire is a hyper-deflationary token that has a smart system built into the ecosystem.MetaInuFIRE automatically sends rewards directly to your wallet by simply holding it.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- ABOUT META INU FIRE -->
+        <!-- <section id="showoff-1" class="py-5 gradient-blue-red text-white" data-aos="zoom-out-right"> -->
+        <section class="py-5 text-white">
+            <div class="container">
+                <div class="row">
+                    <div class="col text-center">
+                        <h2 class="font-weight-bold blue txt-shadow"><?= strtoupper('About Meta Inu '); ?><span class="red txt-shadow"><?= strtoupper('Fire'); ?></span></h2>
+                        <span class="d-block gradient-blue-red mx-auto mb-3" style="width: 150px; height:2px;"></span>
+                        <p class="mt-4" style="font-size: 18px;">Meta Inu Fire soon to become the most popular meme coin to hit the smart chain binance. Fueled by some of the biggest influencers in crypto, MetaInuFire won't go anywhere and make a splash!</p>
+                        <p>MetaInuFire always dreamed of being the first fire to set foot on the moon. MetaInuFire about to take him there! Join us!</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <!-- TOKENOMICS META INU FIRE -->
+        <section id="tokenomics" class=" card py-3 w-75 mx-auto shadow bg-blue">
+            <div class="card-body">
+                <!-- <div class="container"> -->
+                <div class="row">
+                    <div class="col text-center">
+                        <h2 class="font-weight-bold text-warning txt-shadow"><?= strtoupper('Tokenomics'); ?></h2>
+                        <!-- Span is line gradient -->
+                        <!-- <span class="d-block mx-auto mb-3 bg-red" style="width: 150px; height:4px;"></span> -->
+                    </div>
+                </div>
+
+                <div class="row text-center justify-content-between mt-3 text-light">
+                    <div class="col-sm-6 col-md-3 p-3">
+                        <i class="far fa-check-circle fa-3x text-warning"></i>
+                        <h5 class="mt-3">TOTAL FEE 11-15%</h5>
+                    </div>
+
+                    <div class="col-sm-6 col-md-3 p-3">
+                        <i class="far fa-check-circle fa-3x text-warning"></i>
+                        <h5 class="mt-3">BUYBACK/LIQUIDITY POOL 5%</h5>
+                    </div>
+
+                    <div class="col-sm-6 col-md-3 p-3">
+                        <i class="far fa-check-circle fa-3x text-warning"></i>
+                        <h5 class="mt-3">MARKETING & DEVELOPMENT 4%</h5>
+                    </div>
+
+                    <div class="col-sm-6 col-md-3 p-3">
+                        <i class="far fa-check-circle fa-3x text-warning"></i>
+                        <h5 class="mt-3">METAINUFIRE REFLECTION 6%</h5>
+                    </div>
+                </div>
+                <!-- </div> -->
+            </div>
+        </section>
+
+        <!-- RODAMAP META INU FIRE -->
+        <section id="roadmap" class="py-5 text-light">
+            <div class="container mt-4">
+                <h2 class="font-weight-bold text-warning txt-shadow"><?= strtoupper('Road Map'); ?></h2>
+                <!-- Span is line gradient -->
+                <!-- <span class="d-block gradient-blue-red mb-3" style="width: 120px; height:2px;"></span> -->
+                <p>Below are the future plans for development of the Meta Inu Fire</p>
+                <div class="row mt-5 text-left">
+                    <div class="col-md-5 offset-md-2">
+                        <div id="birth">
+                            <h3 class="font-weight-bold text-warning">Birth</h3>
+                            <p>Website launch +</p>
+                            <p>Fair launch +</p>
+                            <p>influencer marketing push +</p>
+                            <p>Coinhunt & Coinsniper +</p>
+                            <p>promos +</p>
+                            <p><a href="#"><strong class="text-warning">800+</strong> HOLDER GROWTH TARGET</a></p>
+                        </div>
+                    </div>
+                    <div class="col-md-5">
+                        <div id="walk">
+                            <h3 class="font-weight-bold text-warning">Walk</h3>
+                            <p>MetaInuFire development</p>
+                            <p>NFT development</p>
+                            <p>Influencer / Celebrity </p>
+                            <p>onboarding</p>
+                            <p><a href="#"><strong class="text-warning">5000+</strong> HOLDER GROWTH TARGET</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-3 text-left">
+                    <div class="col-md-5 offset-md-2">
+                        <div id="crwal">
+                            <h3 class="font-weight-bold text-warning">Crawl</h3>
+                            <p>Smart contract Audits +</p>
+                            <p>CoinMarketCap listing +</p>
+                            <p>CoinGecko listing +</p>
+                            <p>Website redesign +</p>
+                            <p><a href="#"><strong class="text-warning">1600+</strong>HOLDER GROWTH TARGET</a></p>
+                        </div>
+                    </div>
+                    <div class="col-md-5">
+                        <div id="walk">
+                            <h3 class="font-weight-bold text-warning">Run</h3>
+                            <p>CEX listings</p>
+                            <p>Iphone 13 giveaway</p>
+                            <p>Solid partnerships</p>
+                            <p>Further marketing plans</p>
+                            <p><a href="#"><strong class="text-warning">10000+</strong> HOLDER GROWTH TARGET</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- GIVEAWAY META INU FIRE -->
+        <section id="giveaway">
+            <div class="container-fluid bg-light gradient-blue-red text-light">
+                <div class="row">
+                    <div class="col-md-6 p-3 p-md-5">
+                        <h2 class="font-weight-bold red txt-shadow mb-3"><?= strtoupper('Project Dynamics'); ?></h2>
+                        <!-- Span is line gradient -->
+                        <!-- <span class="d-block gradient-blue-red mb-3" style="width: 120px; height:2px;"></span> -->
+                        <p><i class="fas fa-check fa-fw text-success mr-2"></i>Smart contracts automatically.</p>
+                        <p><i class="fas fa-check fa-fw text-success mr-2"></i>Strong, professional, experienced and hard working team.</p>
+                        <p><i class="fas fa-check fa-fw text-success mr-2"></i>Buy back Mechanism (hyper deflationary).</p>
+                        <p><i class="fas fa-check fa-fw text-success mr-2"></i>Anti Dump Mechanisms.</p>
+                        <p><i class="fas fa-check fa-fw text-success mr-2"></i>Reflection to Holders and proper use cases.</p>
+                        <p><i class="fas fa-check fa-fw text-success mr-2"></i>Dev tokens locked for at least 6 mounth.</p>
+                        <p><i class="fas fa-check fa-fw text-success mr-2"></i>Rocket AutoBoost & Hyperdeflationary.</p>
+                    </div>
+                    <div class="col-md-6 p-3 p-md-5">
+                        <h1 class="font-weight-bold text-warning txt-shadow mb-3">Iphone 13 Giveaway</h1>
+                        <!-- <span class="d-block gradient-blue-red mb-3" style="width: 120px; height:2px;"></span> -->
+                        <p style="font-size:18px;" class="txt-shadow">We'll ve doing a Iphone 13 giveaway when we rach 10000 Holder</p>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    </div>
+
+    <!--FOOTER -->
+    <footer id="main-footer" class="py-5 text-center text-light">
+        <div class="container text-center">
+            <div class="row">
+                <div class="col">
+                    <h1 class="blue txt-shadow mb-3"><strong>META INU <span class="red">FIRE</span></strong></h3>
+                        <p>Copyright<i class="far fa-copyright mx-1"></i><?= date('Y'); ?> Golden Floki Inu. All Rights Reserved Privacy Policy – Terms & Conditions</p>
+                        <p>THE TEAM IS WORKING LIKE CRAZY IN THE BACK END IN FULL POWER
+                            WE ARE GETTING DM'S FROM EVERY INFLUENCER, TG GROUP, SOCIAL MEDIA ACCOUNTS ETC OUT THERE!
+                        </p>
+                        <p>REQUEST YOU TO READ THE PINNED MESSAGES AND BEAR WITH US TILL THE LAUNCH AND KEEP SHILLING! WHILE WE ARE WORKING CONSTANTLY!
+                            WE HAVE TIMED THIS TO PERFECTION AND ARE GOING TO THE MOON ON THIS ONE TOGETHER!
+                        </p>
+                        <div class="d-flex justify-content-center mt-4">
+                            <div id="telegram" class="mr-3">
+                                <a href="https://t.me/FireMetaInu" class="text-warning" target="_blank">
+                                    <p>TELEGRAM</p>
+                                    <p><i class="fab fa-telegram fa-2x"></i></p>
+                                </a>
+                            </div>
+                            <div id="telegram">
+                                <a href="https://twitter.com/MetaInuFire?t=hggKLoTz0lQe2q1z0l-32A&s=08" class="text-warning" target="_blank">
+                                    <p>TWITTER</p>
+                                    <p><i class="fab fa-twitter-square fa-2x"></i></p>
+                                </a>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="js/jquery-3.3.1.js"></script>
+    <script src="js/popper.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <!-- <script src="js/aos.js"></script> -->
+    <script>
+        // AOS.init({
+        //     once: false,
+        //     duration: 1000,
+        // });
+        $(document).ready(function() {
+            $("a").on('click', function(event) {
+
+                if (this.hash !== "") {
+                    event.preventDefault();
+
+                    var hash = this.hash;
+                    $('html, body').animate({
+                        scrollTop: $(hash).offset().top
+                    }, 800, function() {
+                        window.location.hash = hash;
+                    });
+                }
+
+            });
+        });
+    </script>
+</body>
+
+</html>
